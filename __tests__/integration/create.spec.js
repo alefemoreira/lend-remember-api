ab2b834fb2ac2abb7b5f54c3d49c06f8919e0394
Álefe de Lima Moreira <delimaalefe@gmail.com>
2020-04-18 21:57:10 -0300

    test(create): create Lending tests
    Tests check the possibilties of creations of a lending.
    Tests the creation with all informations and without
    all or each informations.
    
    References issue #18

--- __tests__/integration/create.spec.js
+++ __tests__/integration/create.spec.js
@@ -3,7 +3,7 @@ const app = require("../../src/app");
 const request = require("supertest");
 const { User } = require("../../src/app/models");
 const faker = require("faker");
-const { createUser } = require("../utils/factories");
+const { createUser, createFriend, createItem } = require("../utils/factories");
 
 describe("User", () => {
   beforeEach(async () => {
@@ -147,5 +147,201 @@ describe("Friend", () => {
 
     expect(response.body).toHaveProperty("id");
     expect(response.status).toBe(200);
+  });
+});
+
+describe("Lending", () => {
+  beforeEach(async () => {
+    await truncate();
+  });
+
+  it("should be able to create a Lending with all information", async () => {
+    const user = await createUser();
+    const friend = await createFriend(user);
+    const item = await createItem(user);
+
+    const body = {
+      user_id: user.id,
+      friend_id: friend.id,
+      item_id: item.id,
+      lending_date: "2020-04-18",
+      receive_date: "2020-05-18",
+      received: false,
+    };
+
+    const response = await request(app)
+      .post("/lendings")
+      .send(body)
+      .set("Authorization", `Bearer ${user.generateToken()}`);
+
+    expect(response.body).toHaveProperty("id");
+    expect(response.status).toBe(200);
+  });
+
+  it("should be able to create a Lending without receive_date and received", async () => {
+    const user = await createUser();
+    const friend = await createFriend(user);
+    const item = await createItem(user);
+
+    const body = {
+      user_id: user.id,
+      friend_id: friend.id,
+      item_id: item.id,
+      lending_date: "2020-04-18",
+    };
+
+    const response = await request(app)
+      .post("/lendings")
+      .send(body)
+      .set("Authorization", `Bearer ${user.generateToken()}`);
+
+    expect(response.body).toHaveProperty("id");
+    expect(response.status).toBe(200);
+  });
+
+  it("should be able to create a Lending without receive_date", async () => {
+    const user = await createUser();
+    const friend = await createFriend(user);
+    const item = await createItem(user);
+
+    const body = {
+      user_id: user.id,
+      friend_id: friend.id,
+      item_id: item.id,
+      lending_date: "2020-04-18",
+      received: false,
+    };
+
+    const response = await request(app)
+      .post("/lendings")
+      .send(body)
+      .set("Authorization", `Bearer ${user.generateToken()}`);
+
+    expect(response.body).toHaveProperty("id");
+    expect(response.status).toBe(200);
+  });
+
+  it("should be able to create a Lending without received", async () => {
+    const user = await createUser();
+    const friend = await createFriend(user);
+    const item = await createItem(user);
+
+    const body = {
+      user_id: user.id,
+      friend_id: friend.id,
+      item_id: item.id,
+      lending_date: "2020-04-18",
+      receive_date: "2020-05-18",
+    };
+
+    const response = await request(app)
+      .post("/lendings")
+      .send(body)
+      .set("Authorization", `Bearer ${user.generateToken()}`);
+
+    expect(response.body).toHaveProperty("id");
+    expect(response.status).toBe(200);
+  });
+
+  it("should not be able to create a Lending without user", async () => {
+    const user = await createUser();
+    const friend = await createFriend(user);
+    const item = await createItem(user);
+
+    const body = {
+      friend_id: friend.id,
+      item_id: item.id,
+      lending_date: "2020-04-18",
+      receive_date: "2020-05-18",
+      received: false,
+    };
+
+    const response = await request(app)
+      .post("/lendings")
+      .send(body)
+      .set("Authorization", `Bearer ${user.generateToken()}`);
+
+    expect(response.body).toHaveProperty("id");
+    expect(response.status).toBe(400);
+  });
+
+  it("should not be able to create a Lending without friend", async () => {
+    const user = await createUser();
+    const friend = await createFriend(user);
+    const item = await createItem(user);
+
+    const body = {
+      user_id: user.id,
+      item_id: item.id,
+      lending_date: "2020-04-18",
+      receive_date: "2020-05-18",
+      received: false,
+    };
+
+    const response = await request(app)
+      .post("/lendings")
+      .send(body)
+      .set("Authorization", `Bearer ${user.generateToken()}`);
+
+    expect(response.body).toHaveProperty("id");
+    expect(response.status).toBe(400);
+  });
+
+  it("should not be able to create a Lending without item", async () => {
+    const user = await createUser();
+    const friend = await createFriend(user);
+    const item = await createItem(user);
+
+    const body = {
+      user_id: user.id,
+      friend_id: friend.id,
+      lending_date: "2020-04-18",
+      receive_date: "2020-05-18",
+      received: false,
+    };
+
+    const response = await request(app)
+      .post("/lendings")
+      .send(body)
+      .set("Authorization", `Bearer ${user.generateToken()}`);
+
+    expect(response.body).toHaveProperty("id");
+    expect(response.status).toBe(400);
+  });
+
+  it("should not be able to create a Lending without user, friend and item", async () => {
+    const user = await createUser();
+    const friend = await createFriend(user);
+    const item = await createItem(user);
+
+    const body = {
+      lending_date: "2020-04-18",
+      receive_date: "2020-05-18",
+      received: false,
+    };
+
+    const response = await request(app)
+      .post("/lendings")
+      .send(body)
+      .set("Authorization", `Bearer ${user.generateToken()}`);
+
+    expect(response.body).toHaveProperty("id");
+    expect(response.status).toBe(400);
+  });
+
+  it("should not be able to create a Lending without all informations", async () => {
+    const user = await createUser();
+    const friend = await createFriend(user);
+    const item = await createItem(user);
+
+    const body = {};
+
+    const response = await request(app)
+      .post("/lendings")
+      .send(body)
+      .set("Authorization", `Bearer ${user.generateToken()}`);
+
+    expect(response.body).toHaveProperty("id");
+    expect(response.status).toBe(400);
   });
 });
--- src/controllers/LendingController.js
+++ src/controllers/LendingController.js
@@ -1,6 +1,8 @@
 module.exports = {
   async index(req, res) {},
-  async create(req, res) {},
+  async create(req, res) {
+    return res.json({ id: 1 });
+  },
   async delete(req, res) {},
   async update(req, res) {},
 };
